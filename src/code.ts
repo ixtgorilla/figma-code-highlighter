f9b58c53f7d26f2fa0c4b8486f3d24f9b7dc0bd5
ixtgorilla <ixtgorilla@gmail.com>
2019-10-13 00:50:16 +0900

    setup xpathes

--- src/code.ts
+++ src/code.ts
@@ -1,16 +1,40 @@
 import { highlightAuto } from "highlight.js";
+// import xpath from "xpath-ts";
+const xpath = require("xpath");
+const dom = require("xmldom").DOMParser;
+
+declare function require(path: string): any;
 
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg => {
   console.log(figma.currentPage.selection);
+
   figma.currentPage.selection &&
     figma.currentPage.selection.map((item, index) => {
       if (item.type == "TEXT") {
         let itm: TextNode = item;
         console.log("わわお？");
 
-        console.log(highlightAuto(itm.characters, ["typescript"]));
+        const result = highlightAuto(itm.characters, ["typescript"]);
+        const str: string = `<div>${result.value}</div>`;
+        // console.log(result);
+        // console.log(document);
+        // let htmlObject = document.createElement("div");
+        // htmlObject.innerHTML = '<div id="myDiv"></div>';
+
+        const xml = "<book><title>Harry Potter</title></book>";
+        console.log(xml);
+        const doc = new dom().parseFromString(str);
+        console.log(doc);
+        var nodes = xpath.select("//", doc);
+        console.log(nodes);
+
+        // console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
+        // console.log("Node: " + nodes[0].toString());
+        // console.log(document.evaluate("//div", htmlObject));
+        // console.log(wa);
+        // console.log(htmlObject);
       }
     });
 
