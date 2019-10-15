88d98ff2f2567becc7a1f8e8461f1abd4f1de601
ixtgorilla <ixtgorilla@gmail.com>
2019-10-15 13:50:37 +0900

    Add index in colorSchema

---
+++ src/colorSchema/index.ts
@@ -0,0 +1,181 @@
+import a11yDark from "./a11yDark";
+import a11yLight from "./a11yLight";
+import agate from "./agate";
+import anOldHope from "./anOldHope";
+import androidstudio from "./androidstudio";
+import arduinoLight from "./arduinoLight";
+import arta from "./arta";
+import ascetic from "./ascetic";
+import atelierCaveDark from "./atelierCaveDark";
+import atelierCaveLight from "./atelierCaveLight";
+import atelierDuneDark from "./atelierDuneDark";
+import atelierDuneLight from "./atelierDuneLight";
+import atelierEstuaryDark from "./atelierEstuaryDark";
+import atelierEstuaryLight from "./atelierEstuaryLight";
+import atelierForestDark from "./atelierForestDark";
+import atelierForestLight from "./atelierForestLight";
+import atelierHeathDark from "./atelierHeathDark";
+import atelierHeathLight from "./atelierHeathLight";
+import atelierLakesideDark from "./atelierLakesideDark";
+import atelierLakesideLight from "./atelierLakesideLight";
+import atelierPlateauDark from "./atelierPlateauDark";
+import atelierPlateauLight from "./atelierPlateauLight";
+import atelierSavannaDark from "./atelierSavannaDark";
+import atelierSavannaLight from "./atelierSavannaLight";
+import atelierSeasideDark from "./atelierSeasideDark";
+import atelierSeasideLight from "./atelierSeasideLight";
+import atelierSulphurpoolDark from "./atelierSulphurpoolDark";
+import atelierSulphurpoolLight from "./atelierSulphurpoolLight";
+import atomOneDark from "./atomOneDark";
+import atomOneDarkReasonable from "./atomOneDarkReasonable";
+import atomOneLight from "./atomOneLight";
+import brownPaper from "./brownPaper";
+import codepenEmbed from "./codepenEmbed";
+import colorBrewer from "./colorBrewer";
+import darcula from "./darcula";
+import dark from "./dark";
+import defaultColor from "./defaultColor";
+import docco from "./docco";
+import dracula from "./dracula";
+import far from "./far";
+import foundation from "./foundation";
+import github from "./github";
+import githubGist from "./githubGist";
+import gml from "./gml";
+import googlecode from "./googlecode";
+import gorillaColorSchema from "./gorillaColorSchema";
+import grayscale from "./grayscale";
+import gruvboxDark from "./gruvboxDark";
+import gruvboxLight from "./gruvboxLight";
+import hopscotch from "./hopscotch";
+import hybrid from "./hybrid";
+import idea from "./idea";
+import irBlack from "./irBlack";
+import isblEditorDark from "./isblEditorDark";
+import isblEditorLight from "./isblEditorLight";
+import kimbieDark from "./kimbieDark";
+import kimbieLight from "./kimbieLight";
+import lightfair from "./lightfair";
+import magula from "./magula";
+import monoBlue from "./monoBlue";
+import monokai from "./monokai";
+import monokaiSublime from "./monokaiSublime";
+import nord from "./nord";
+import obsidian from "./obsidian";
+import ocean from "./ocean";
+import paraisoDark from "./paraisoDark";
+import paraisoLight from "./paraisoLight";
+import pojoaque from "./pojoaque";
+import purebasic from "./purebasic";
+import qtcreatorDark from "./qtcreatorDark";
+import qtcreatorLight from "./qtcreatorLight";
+import railscasts from "./railscasts";
+import rainbow from "./rainbow";
+import routeros from "./routeros";
+import schoolBook from "./schoolBook";
+import shadesOfPurple from "./shadesOfPurple";
+import solarizedDark from "./solarizedDark";
+import solarizedLight from "./solarizedLight";
+import sunburst from "./sunburst";
+import tomorrow from "./tomorrow";
+import tomorrowNight from "./tomorrowNight";
+import tomorrowNightBlue from "./tomorrowNightBlue";
+import tomorrowNightBright from "./tomorrowNightBright";
+import tomorrowNightEighties from "./tomorrowNightEighties";
+import vs from "./vs";
+import vs2015 from "./vs2015";
+import xcode from "./xcode";
+import xt256 from "./xt256";
+import zenburn from "./zenburn";
+
+export {
+  a11yDark,
+  a11yLight,
+  agate,
+  anOldHope,
+  androidstudio,
+  arduinoLight,
+  arta,
+  ascetic,
+  atelierCaveDark,
+  atelierCaveLight,
+  atelierDuneDark,
+  atelierDuneLight,
+  atelierEstuaryDark,
+  atelierEstuaryLight,
+  atelierForestDark,
+  atelierForestLight,
+  atelierHeathDark,
+  atelierHeathLight,
+  atelierLakesideDark,
+  atelierLakesideLight,
+  atelierPlateauDark,
+  atelierPlateauLight,
+  atelierSavannaDark,
+  atelierSavannaLight,
+  atelierSeasideDark,
+  atelierSeasideLight,
+  atelierSulphurpoolDark,
+  atelierSulphurpoolLight,
+  atomOneDark,
+  atomOneDarkReasonable,
+  atomOneLight,
+  brownPaper,
+  codepenEmbed,
+  colorBrewer,
+  darcula,
+  dark,
+  defaultColor,
+  docco,
+  dracula,
+  far,
+  foundation,
+  github,
+  githubGist,
+  gml,
+  googlecode,
+  gorillaColorSchema,
+  grayscale,
+  gruvboxDark,
+  gruvboxLight,
+  hopscotch,
+  hybrid,
+  idea,
+  irBlack,
+  isblEditorDark,
+  isblEditorLight,
+  kimbieDark,
+  kimbieLight,
+  lightfair,
+  magula,
+  monoBlue,
+  monokai,
+  monokaiSublime,
+  nord,
+  obsidian,
+  ocean,
+  paraisoDark,
+  paraisoLight,
+  pojoaque,
+  purebasic,
+  qtcreatorDark,
+  qtcreatorLight,
+  railscasts,
+  rainbow,
+  routeros,
+  schoolBook,
+  shadesOfPurple,
+  solarizedDark,
+  solarizedLight,
+  sunburst,
+  tomorrow,
+  tomorrowNight,
+  tomorrowNightBlue,
+  tomorrowNightBright,
+  tomorrowNightEighties,
+  vs,
+  vs2015,
+  xcode,
+  xt256,
+  zenburn
+};
